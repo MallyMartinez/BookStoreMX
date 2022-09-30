041895367ec03d40cf95a360a66d6ae6dd566724
Carlos Chavoya <carlos.chavoya4676@alumnos.udg.mx>
2022-09-30 03:22:54 -0500

    PopUp compra (total de compra)

--- js/popUp.js
+++ js/popUp.js
@@ -1,24 +1,41 @@
 var btnAbrirPopUp = document.getElementById("btn_comprar"),
     overlay = document.getElementById("overlay"),
     popup = document.getElementById("popup"),
-    btnCerrarPopup = document.getElementById("btn-cerrar-popup")
+    btnCerrarPopup = document.getElementById("btn-cerrar-popup"),
+    precioCompra = document.getElementById("precioUnitario"),
+    totalCompra = document.getElementById("totalCompra"),
+    cantidad =document.getElementById("range"),
+    btnConfirmaCompra = document.getElementById("btnConfirmaCompra"),
+    btnCambios = document.getElementById("btnAplicarCambios");
 
 btnAbrirPopUp.addEventListener('click', function(){
     overlay.classList.add('active');
     popup.classList.add('active');
     cambiarImagenJS();
-
+    ObtenerTotalCompra();
 });
 
 btnCerrarPopup.addEventListener('click', function(){
     overlay.classList.remove('active');
     popup.classList.remove('active');
+
 });
+
+btnConfirmaCompra.addEventListener('click', function(){
+    
+});
+
+btnCambios.addEventListener('click',function(){
+    ObtenerTotalCompra();
+});
+   
 
 function cambiarImagenJS(){
     document.getElementById("img_popup").src = document.getElementById("img_compra").src
 }
 
-
-
+function ObtenerTotalCompra(){
+    var total = Number(precioCompra.innerHTML)*cantidad.value
+    totalCompra.innerHTML = String(total)
+}
 
